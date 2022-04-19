e505d0ff3d275631f85aec87ed9bd0cb1db8040e
Yareslav <80545801+Yareslav@users.noreply.github.com>
2022-04-19 19:33:11 +0300

    new2 branch first commit

--- code.py
+++ code.py
@@ -1,9 +1,7 @@
 print("hello world");
 do {
 	print(121212);
-<<<<<<< HEAD
-=======
 } while (true);
 this is a change on branch hello
 this is second change on branch hello
->>>>>>> hello
+this is new2 branch
